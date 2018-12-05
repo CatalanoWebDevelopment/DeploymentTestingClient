e91d074f8a734f0d7cb1de703b49f58f484abb65
Parker K Catalano <parkerkcatalano@Admins-MacBook-Pro.local>
2018-12-05 14:43:40 -0500

    Component will mount not working, deleted

--- src/components/UserPage/UserHome.js
+++ src/components/UserPage/UserHome.js
@@ -1,56 +1,68 @@
 import React, { Component } from "react";
-import "../Styles.css"
+import "../Styles.css";
 import Grid from "@material-ui/core/Grid";
 import Login from "./UserLogin";
 import Register from "./UserRegister";
 import Greeting from "./UserGreeting";
 
 export default class UserHome extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            isLoginOpen: true,
-            isRegisterOpen: false,
-            isGreetingOpen: false
-        }
-    }
+    this.state = {
+      isLoginOpen: true,
+      isRegisterOpen: false,
+      isGreetingOpen: false
+    };
+  }
 
-    renderLogin = () => {
-        this.setState({
-            isLoginOpen: true,
-            isRegisterOpen: false,
-            isGreetingOpen: false
-        })
-    }
+  renderLogin = () => {
+    this.setState({
+      isLoginOpen: true,
+      isRegisterOpen: false,
+      isGreetingOpen: false
+    });
+  };
 
-    renderRegister = () => {
-        this.setState({
-            isLoginOpen: false,
-            isRegisterOpen: true,
-            isGreetingOpen: false
-        })
-    }
+  renderRegister = () => {
+    this.setState({
+      isLoginOpen: false,
+      isRegisterOpen: true,
+      isGreetingOpen: false
+    });
+  };
 
-    renderGreeting = () => {
-        this.setState({
-            isLoginOpen: false,
-            isRegisterOpen: false,
-            isGreetingOpen: true
-        })
-    }
+  renderGreeting = () => {
+    this.setState({
+      isLoginOpen: false,
+      isRegisterOpen: false,
+      isGreetingOpen: true
+    });
+  };
 
-    render() {
-        return(
-            <Grid container spacing={8}>
-                <Grid item xs={12}>
-                    {this.state.isLoginOpen === true && <Login renderRegister={this.renderRegister} renderLinks={this.props.renderLinks} renderGreeting={this.renderGreeting} />}
+  render() {
+    return (
+      <Grid container spacing={8}>
+        <Grid item xs={12}>
+          {this.state.isLoginOpen === true && (
+            <Login
+              renderRegister={this.renderRegister}
+              renderLinks={this.props.renderLinks}
+              renderGreeting={this.renderGreeting}
+            />
+          )}
 
-                    {this.state.isRegisterOpen === true && <Register renderLogin={this.renderLogin} renderLinks={this.props.renderLinks} renderGreeting={this.renderGreeting} />}
+          {this.state.isRegisterOpen === true && (
+            <Register
+              renderLogin={this.renderLogin}
+              renderLinks={this.props.renderLinks}
+              renderGreeting={this.renderGreeting}
+            />
+          )}
 
-                    {this.state.isGreetingOpen === true && <Greeting />}
-                </Grid>
-            </Grid>
-        )
-    }
+          {this.state.isGreetingOpen === true && <Greeting />}
+        </Grid>
+      </Grid>
+    );
+  }
 }
